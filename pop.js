e1a818160c143b5de983d4f05099310b30f5866d
zelosn <60331414+zelosn@users.noreply.github.com>
2020-01-27 18:27:38 +0200

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,11 +1,10 @@
 <script type="text/javascript">
-    var ad_idzone = "3299706",
-    ad_popup_fallback = true,
+    var ad_idzone = "3502383",
+    ad_popup_fallback = false,
     ad_popup_force = false,
-    ad_new_tab = true,
-    ad_frequency_period = 30,
+    ad_new_tab = false,
+    ad_frequency_period = 60,
     ad_frequency_count = 1,
-    ad_trigger_method = 2,
-    ad_trigger_class = "videos-list";
+    ad_trigger_method = 1;
 </script>
 <script type="text/javascript" src="https://a.exosrv.com/popunder1000.js"></script>
