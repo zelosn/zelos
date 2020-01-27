ec50c230e69bb5c5d3e5408c6e33ac7dae83cedf
zelosn <60331414+zelosn@users.noreply.github.com>
2020-01-27 03:02:34 +0200

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    var ad_idzone = "3299706",
+    ad_popup_fallback = true,
+    ad_popup_force = false,
+    ad_new_tab = true,
+    ad_frequency_period = 30,
+    ad_frequency_count = 1,
+    ad_trigger_method = 2,
+    ad_trigger_class = "videos-list";
+</script>
+<script type="text/javascript" src="https://a.exosrv.com/popunder1000.js"></script>
